d5e0c0475cc61dadf2b87394e323225382e6c026
Dar Levy <darlevy8@gmail.com>
2024-07-09 17:53:38 +0300

    Decorate class with blp

--- routes/ask.py
+++ routes/ask.py
@@ -3,6 +3,8 @@ from flask_smorest import Blueprint, abort
 
 blp = Blueprint('ask', __name__, description="Ask a question & pass to OpenAI")
 
+
+@blp.route('/ask')
 class Ask(MethodView):
     def post(self):
         pass
