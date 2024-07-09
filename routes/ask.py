e4ef22a7f27e1770abc5f1f442a6037c5c8bd011
Dar Levy <darlevy8@gmail.com>
2024-07-09 19:44:18 +0300

    Decorate Ask route with QuestionSchema argument

--- routes/ask.py
+++ routes/ask.py
@@ -1,13 +1,14 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from schemas.ask_schema import AskSchema
+from schemas.ask_schema import AskSchema, QuestionSchema
 
 blp = Blueprint('ask', __name__, description="Ask a question & pass to OpenAI")
 
 
 @blp.route('/ask')
 class Ask(MethodView):
+    @blp.arguments(QuestionSchema)
     @blp.response(200, AskSchema)
     def post(self):
-        return {"question": "Some question", "answer": "Some answer"}
+        return {"id": 1, "question": "Some question", "answer": "Some answer"}
