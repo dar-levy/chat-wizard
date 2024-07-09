b9eae7c42a3b073dce8663a6f9d6201f7bb85a7b
Dar Levy <darlevy8@gmail.com>
2024-07-09 18:25:02 +0300

    Wrap with swagger

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_smorest import Api
+from routes.ask import blp as ask_blueprint
 
 app = Flask(__name__)
 
@@ -6,7 +8,12 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 app.config["API_TITLE"] = "ChatWizard REST API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
 api = Api(app)
+
 api.register_blueprint(ask_blueprint)
 
 
