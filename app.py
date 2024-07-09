80c89145b96b2e5ad2127a7c9a79f091b313e429
Dar Levy <darlevy8@gmail.com>
2024-07-09 18:24:42 +0300

    Register ask blueprint to app

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "ChatWizard REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+api = Api(app)
+api.register_blueprint(ask_blueprint)
 
 
 if __name__ == '__main__':
