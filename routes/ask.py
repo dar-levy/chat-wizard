5c15692bd40da590c5dcf9ce7c5251d78a46037c
Dar Levy <darlevy8@gmail.com>
2024-07-09 19:40:27 +0300

    Decorate post with response by AskSchema

--- routes/ask.py
+++ routes/ask.py
@@ -1,10 +1,13 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
+from schemas.ask_schema import AskSchema
+
 blp = Blueprint('ask', __name__, description="Ask a question & pass to OpenAI")
 
 
 @blp.route('/ask')
 class Ask(MethodView):
+    @blp.response(200, AskSchema)
     def post(self):
         return {"question": "Some question", "answer": "Some answer"}
