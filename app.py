1222565c4df220c1db9bd6c1aa508bd67003841d
Dar Levy <darlevy8@gmail.com>
2024-07-10 00:45:24 +0300

    Interact with sqlite for simplicity until postgres

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from flask_smorest import Api
 from routes.ask import blp as ask_blueprint
-
-app = Flask(__name__)
-
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "ChatWizard REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-
-api = Api(app)
-
-api.register_blueprint(ask_blueprint)
+from db import db
+import models
 
 
-if __name__ == '__main__':
-    app.run()
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "ChatWizard REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
+
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ask_blueprint)
