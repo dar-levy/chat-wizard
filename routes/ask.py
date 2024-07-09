161bc97d50d939b37f0abb1140f279c4b33cc261
Dar Levy <darlevy8@gmail.com>
2024-07-09 17:52:58 +0300

    Configure blp

--- routes/ask.py
+++ routes/ask.py
@@ -1,5 +1,7 @@
 from flask.views import MethodView
+from flask_smorest import Blueprint, abort
 
+blp = Blueprint('ask', __name__, description="Ask a question & pass to OpenAI")
 
 class Ask(MethodView):
     def post(self):
