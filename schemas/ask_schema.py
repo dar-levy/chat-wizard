62e5b5c92c413a7439dea19e4ce5adf55e2e17fa
Dar Levy <darlevy8@gmail.com>
2024-07-09 19:43:45 +0300

    Create QuestionSchema

--- schemas/ask_schema.py
+++ schemas/ask_schema.py
@@ -3,5 +3,9 @@ from marshmallow import Schema, fields, validate
 
 class AskSchema(Schema):
     id = fields.Int(dump_only=True)
-    question = fields.Str(required=True, validate=validate.Length(max=100))
+    question = fields.Str(required=True)
     answer = fields.Str(required=True)
+
+
+class QuestionSchema(Schema):
+    question = fields.Str(required=True, validate=validate.Length(max=100))
