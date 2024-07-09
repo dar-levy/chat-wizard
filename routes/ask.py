3caff0abf0981a4afe898e639669decf2f945d6e
Dar Levy <darlevy8@gmail.com>
2024-07-09 17:59:24 +0300

    Dummy response until openAI feature

--- routes/ask.py
+++ routes/ask.py
@@ -7,4 +7,4 @@ blp = Blueprint('ask', __name__, description="Ask a question & pass to OpenAI")
 @blp.route('/ask')
 class Ask(MethodView):
     def post(self):
-        pass
+        return {"question": "Some question", "answer": "Some answer"}
