fb7143d8a8a226d7d1ccf38a80d317f7385c67a3
Dar Levy <darlevy8@gmail.com>
2024-07-10 02:14:26 +0300

    Create openAI service

---
+++ services/openai_service.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="some falsy key")
+
+
+def ask_openai(question):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+    )
+    answer = response.choices[0].message.content.strip()
+
+    return answer
