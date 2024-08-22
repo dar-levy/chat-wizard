ff3571991b522cc37660eacd16679474d1e87870
Dar Levy <darlevy8@gmail.com>
2024-08-22 11:42:39 +0300

    Fix call to property of jwt

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def create_app(db_url=None):
     Migrate(app, db)
     api = Api(app)
 
-    app.config("JWT_SECRET_KEY") == os.getenv("JWT_SECRET_KEY")
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
 
     api.register_blueprint(ask_blueprint)
